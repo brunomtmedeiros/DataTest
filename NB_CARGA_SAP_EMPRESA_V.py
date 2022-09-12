04b526192625820f9be9e34d06606d7da864439e
bruno.medeiros@slcagricola.com.br <bruno.medeiros@slcagricola.com.br>
2022-09-12 12:24:58 +0000

    criacao de golds

---
+++ NB_CARGA_SAP_EMPRESA_V.py
@@ -0,0 +1,63 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ### NB_CARGA_SAP_EMPRESA ###
+# MAGIC Tabela com informações de empresas do SAP com a visao de suprimentos.
+# MAGIC 
+# MAGIC 
+# MAGIC Data de criação: 12/09/2022 -- Responsavel: Bruno Martins Medeiros
+
+# COMMAND ----------
+
+from pyspark.sql.functions import *
+
+# COMMAND ----------
+
+# DBTITLE 1,Criaçao de variaveis
+owner = 'DVRY_SUPRIMENTOS'
+table = 'EMPRESA'
+pthDestino = pthDestino = '/mnt/dvryzone-slc/' + owner + '/' + table
+
+print('owner ----> '+owner)
+print('table ----> '+table)
+print('pthDestino ----> '+pthDestino)
+
+# COMMAND ----------
+
+# DBTITLE 1,Criaçao de schema
+spark.sql("CREATE DATABASE IF NOT EXISTS " + owner)
+
+# COMMAND ----------
+
+# DBTITLE 1,Importa funções genéricas
+# MAGIC %run "/Engenharia/Functions/Functions_global"
+
+# COMMAND ----------
+
+# DBTITLE 1,Dataset de saída
+def gera_tabela_saida():
+  df = spark.sql(f"""
+SELECT DISTINCT
+       BUKRS AS EMPRESA,
+       BUTXT AS NOME_EMPRESA
+FROM TRUSTED_SAP.T001
+""")
+  return df
+
+# COMMAND ----------
+
+df = gera_tabela_saida()
+display(df)
+
+# COMMAND ----------
+
+# DBTITLE 1,Orquestraçao de carga
+# geraçao de tabela de saida
+df = gera_tabela_saida()
+
+#Grava saida de dataframes
+status = False
+x = 0
+while status == False and x <10:
+  status = insere_dados_tabela_populada_dvry(df, owner, table, 'overwrite', 'parquet', pthDestino)
+  x=x+1
+  print(status)
